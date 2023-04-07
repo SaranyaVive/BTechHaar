f66612423c0c31b4a1d789ccf768c50e0022d0a2
boopathy <boopathycute007@gmail.com>
2023-04-07 21:20:52 +0530

    LoginPage Added

--- BTechHaar/Controllers/Web/HomeController.cs
+++ BTechHaar/Controllers/Web/HomeController.cs
@@ -1,15 +1,20 @@
 ﻿using BTechHaar.Models.Models.Web;
 using BTechHaar.Main.Services;
 using Microsoft.AspNetCore.Mvc;
+using BTechHaar.Main.Helpers;
+using Microsoft.Extensions.Options;
+using Microsoft.CodeAnalysis.Options;
 
 namespace BTechHaar.Main.Controllers.Web
 {
     public class HomeController : Controller
     {
         private readonly IUserLogService _userLogService;
-        public HomeController(IUserLogService userLogService)
+        private readonly User _user;
+        public HomeController(IUserLogService userLogService, IOptions<User> user)
         {
             _userLogService = userLogService;
+            _user = user.Value;
         }
 
         public async Task<IActionResult> Index()
@@ -17,6 +22,26 @@ namespace BTechHaar.Main.Controllers.Web
             List<UserLogsVM> logs = new List<UserLogsVM>();
             logs = await _userLogService.GetUserLogs();
             return View(logs);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Authenticate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Authenticate(User user)
+        {
+            
+            if (_user.username == user.username && _user.password == user.password)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Invalid login.");
+                return View(user);
+            }
         }
     }
 }
---
+++ BTechHaar/Helpers/User.cs
@@ -0,0 +1,8 @@
+﻿namespace BTechHaar.Main.Helpers
+{
+    public class User
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
