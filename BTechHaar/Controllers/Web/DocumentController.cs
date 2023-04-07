90477cdbd458f451c831fe3903ffbf254009d081
SaranyaVivek <86953985+SaranyaVive@users.noreply.github.com>
2023-04-07 18:29:17 +0530

    Added Terms and Conditions.

---
+++ BTechHaar/Controllers/Web/DocumentController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BTechHaar.Main.Controllers.Web
+{
+    public class DocumentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult TermAndConditions()
+        {
+            return View();
+        }
+        public IActionResult PrivacyPolicy()
+        {
+            return View();
+        }
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+    }
+}
